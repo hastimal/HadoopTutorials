88f6f1e88b6eff0f93136ecd310929a55f6305ef
hastimal <hjdk4@mail.umkc.edu>
2016-03-09 20:37:52 -0600

    Initial WordCount
    Added WC

---
+++ HadoopTutorials/src/com/bigdata/hadoop/WordCount.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.bigdata.hadoop;
+
+/**
+ * @author hastimal
+ *
+ * Created by hastimal jangid @ Mar 9, 2016 
+ * WordCount.java
+ */
+public class WordCount {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
